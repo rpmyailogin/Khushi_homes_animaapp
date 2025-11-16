09cab30fc3cae9304c988ee7494a2308b44e39b5
Ritz <rp.myailogin@gmail.com>
2025-11-16 16:34:04 +1100

    Added package-lock.json

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { ScrollToTop } from "@/components/ScrollToTop";
 
 export const App = () => {
   return (
-    <body className="text-zinc-800 text-base not-italic normal-nums font-normal accent-auto bg-white box-border caret-transparent block tracking-[normal] leading-[27.2px] list-outside list-disc min-h-full pointer-events-auto text-start indent-[0px] normal-case visible border-separate font-public_sans">
+    <div className="text-zinc-800 text-base not-italic normal-nums font-normal accent-auto bg-white box-border caret-transparent block tracking-[normal] leading-[27.2px] list-outside list-disc min-h-full pointer-events-auto text-start indent-[0px] normal-case visible border-separate font-public_sans">
       <Navbar />
       <Hero />
       <ProjectsSection />
@@ -15,6 +15,6 @@ export const App = () => {
       <div className="fixed box-border caret-transparent contents z-[2147483647] left-0 top-0">
         <div className="caret-transparent"></div>
       </div>
-    </body>
+    </div>
   );
 };
