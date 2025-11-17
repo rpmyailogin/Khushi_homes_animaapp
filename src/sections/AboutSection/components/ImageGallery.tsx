e371c52952e1790cc0125f32339b4c4cc00936bd
Ritz <rp.myailogin@gmail.com>
2025-11-17 21:54:16 +1100

    Updated index.tsx

--- src/sections/AboutSection/components/ImageGallery.tsx
+++ src/sections/AboutSection/components/ImageGallery.tsx
@@ -3,17 +3,16 @@ export const ImageGallery = () => {
     <div className="box-border caret-transparent flex gap-x-4 md:gap-x-5">
       <div className="relative bg-gray-100 box-border caret-transparent w-48 h-48 overflow-hidden">
         <img
-          src="https://cdn.prod.website-files.com/679b678d080aadecaa78b6ac/67a0915d484acc07916249a8_about-us-02.jpg"
+          src="https://images.pexels.com/photos/29672214/pexels-photo-29672214.jpeg"
           alt="About Image 1"
           className="box-border caret-transparent inline-block h-full max-w-full object-cover w-full"
         />
       </div>
       <div className="relative bg-gray-100 box-border caret-transparent w-48 h-48 overflow-hidden">
         <img
-          src="https://cdn.prod.website-files.com/679b678d080aadecaa78b6ac/67a0915d79eeea9dd5fc58aa_about-us-01.jpg"
+          src="https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg"
           alt="About Image 2"
           className="box-border caret-transparent inline-block h-full max-w-full object-cover w-full"
-          loading="eager"
         />
       </div>
     </div>
