2aebe66047b8514b747be8790cfb0748f2d6f012
Ritz <rp.myailogin@gmail.com>
2025-11-17 20:37:28 +1100

    Updated index.tsx

--- src/sections/AboutSection/components/ImageGallery.tsx
+++ src/sections/AboutSection/components/ImageGallery.tsx
@@ -4,7 +4,15 @@ export const ImageGallery = () => {
       <div className="relative bg-gray-100 box-border caret-transparent w-full overflow-hidden">
         <img
           src="https://cdn.prod.website-files.com/679b678d080aadecaa78b6ac/67a0915d484acc07916249a8_about-us-02.jpg"
-          alt="About Image"
+          alt="About Image 1"
+          className="box-border caret-transparent inline-block h-64 max-w-full object-cover w-full md:h-80"
+        />
+        <div className="absolute bg-neutral-900 bottom-[-1%] box-border caret-transparent left-[-1%] right-[-1%] top-[-1%] translate-y-[100.0%]"></div>
+      </div>
+      <div className="relative bg-gray-100 box-border caret-transparent w-full overflow-hidden">
+        <img
+          src="https://cdn.prod.website-files.com/679b678d080aadecaa78b6ac/67a0915d79eeea9dd5fc58aa_about-us-01.jpg"
+          alt="About Image 2"
           className="box-border caret-transparent inline-block h-64 max-w-full object-cover w-full md:h-80"
         />
         <div className="absolute bg-neutral-900 bottom-[-1%] box-border caret-transparent left-[-1%] right-[-1%] top-[-1%] translate-y-[100.0%]"></div>
