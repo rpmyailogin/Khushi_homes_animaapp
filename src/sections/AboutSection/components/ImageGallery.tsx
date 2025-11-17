67835f8679107a1e930549e0b3d554568cfc0fcd
Ritz <rp.myailogin@gmail.com>
2025-11-17 21:56:59 +1100

    Updated ImageGallery.tsx

--- src/sections/AboutSection/components/ImageGallery.tsx
+++ src/sections/AboutSection/components/ImageGallery.tsx
@@ -3,14 +3,14 @@ export const ImageGallery = () => {
     <div className="box-border caret-transparent flex gap-x-4 md:gap-x-5">
       <div className="relative bg-gray-100 box-border caret-transparent w-48 h-48 overflow-hidden">
         <img
-          src="https://images.pexels.com/photos/29672214/pexels-photo-29672214.jpeg"
+          src="https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg?auto=compress&cs=tinysrgb&w=600"
           alt="About Image 1"
           className="box-border caret-transparent inline-block h-full max-w-full object-cover w-full"
         />
       </div>
       <div className="relative bg-gray-100 box-border caret-transparent w-48 h-48 overflow-hidden">
         <img
-          src="https://images.pexels.com/photos/323780/pexels-photo-323780.jpeg"
+          src="https://images.pexels.com/photos/106399/pexels-photo-106399.jpeg?auto=compress&cs=tinysrgb&w=600"
           alt="About Image 2"
           className="box-border caret-transparent inline-block h-full max-w-full object-cover w-full"
         />
