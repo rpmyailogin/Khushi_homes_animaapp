2f5795d71b122d4dbcbfd077760afa9db7d60f8a
Ritz <rp.myailogin@gmail.com>
2025-11-17 22:43:19 +1100

    Updated index.tsx

--- src/sections/AboutSection/index.tsx
+++ src/sections/AboutSection/index.tsx
@@ -3,9 +3,9 @@ import { ImageGallery } from "@/sections/AboutSection/components/ImageGallery";
 
 export const AboutSection = () => {
   return (
-    <section className="bg-black box-border caret-transparent py-10 md:py-16">
+    <section className="bg-black box-border caret-transparent py-6 md:py-16">
       <div className="box-border caret-transparent max-w-full w-full mx-auto px-5 md:max-w-[1336px]">
-        <div className="flex items-start gap-x-16 mb-8 md:mb-10">
+        <div className="flex items-start gap-x-16 mb-5 md:mb-10">
           <div className="text-white text-sm items-center box-border caret-transparent gap-x-2 flex justify-start leading-[16.8px] uppercase flex-shrink-0">
             <div className="box-border caret-transparent gap-x-1 flex">
               <div className="bg-white/10 box-border caret-transparent max-h-1.5 max-w-1.5 min-h-1.5 min-w-1.5"></div>
@@ -17,7 +17,7 @@ export const AboutSection = () => {
           <ImageGallery />
         </div>
 
-        <div className="ml-[170px]">
+        <div className="ml-0 md:ml-[170px]">
           <AboutContent />
         </div>
       </div>
