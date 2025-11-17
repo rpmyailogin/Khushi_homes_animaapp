8c8fbc935334cb740e19cfd676c8863d8ef6fdde
Ritz <rp.myailogin@gmail.com>
2025-11-17 20:33:49 +1100

    Updated ServiceGrid.tsx

--- src/sections/ServicesSection/components/ServiceGrid.tsx
+++ src/sections/ServicesSection/components/ServiceGrid.tsx
@@ -4,7 +4,7 @@ export const ServiceGrid = () => {
   return (
     <div
       role="list"
-      className="box-border caret-transparent grid auto-cols-[1fr] grid-cols-[1fr] grid-rows-[auto] gap-y-10 md:grid-cols-[1fr_1fr_1fr]"
+      className="box-border caret-transparent grid auto-cols-[1fr] grid-cols-[1fr] grid-rows-[auto] gap-y-10 md:grid-cols-[1fr_1fr] md:gap-x-0"
     >
       <ServiceCard
         title="New Home Construction"
@@ -18,13 +18,6 @@ export const ServiceGrid = () => {
         iconUrl="https://cdn.prod.website-files.com/679b74f316932fb3b1e01c07/679dba41441cc64868b978bd_ic-foundation.svg"
         description="Breathe new life into your existing property with comprehensive rebuild services. We deliver seamless renovations that enhance both function and value."
         serviceImageUrl="https://cdn.prod.website-files.com/679b74f316932fb3b1e01c07/679dba46477f30fd79c4ca4a_services-thumb-06.jpg"
-        href="/services"
-      />
-      <ServiceCard
-        title="Interior Design & Styling"
-        iconUrl="https://cdn.prod.website-files.com/679b74f316932fb3b1e01c07/679db879eb37a230407186f4_ic-commercial.svg"
-        description="Create beautiful, functional living spaces with our expert interior design services. From selecting premium finishes to optimizing room layouts."
-        serviceImageUrl="https://cdn.prod.website-files.com/679b74f316932fb3b1e01c07/679db87dce7fe13ceb2a6e80_services-thumb-04.jpg"
         href="/services"
       />
       <ServiceCard
