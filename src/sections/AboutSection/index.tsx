673ff9337f108602a2d2d6088e8ad9a9aa61eeb6
Ritz <rp.myailogin@gmail.com>
2025-11-17 21:58:29 +1100

    Updated index.tsx

--- src/sections/AboutSection/index.tsx
+++ src/sections/AboutSection/index.tsx
@@ -5,7 +5,7 @@ export const AboutSection = () => {
   return (
     <section className="bg-black box-border caret-transparent py-10 md:py-16">
       <div className="box-border caret-transparent max-w-full w-full mx-auto px-5 md:max-w-[1336px]">
-        <div className="flex items-start gap-x-8 mb-8 md:mb-10">
+        <div className="flex items-start gap-x-16 mb-8 md:mb-10">
           <div className="text-white text-sm items-center box-border caret-transparent gap-x-2 flex justify-start leading-[16.8px] uppercase flex-shrink-0">
             <div className="box-border caret-transparent gap-x-1 flex">
               <div className="bg-white/10 box-border caret-transparent max-h-1.5 max-w-1.5 min-h-1.5 min-w-1.5"></div>
