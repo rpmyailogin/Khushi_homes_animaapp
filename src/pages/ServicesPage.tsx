cf716d6004d7f8d06bbd07210b57a8434437c10b
Ritz <rp.myailogin@gmail.com>
2025-11-23 16:07:37 +1100

    Updated ServicesPage.tsx

--- src/pages/ServicesPage.tsx
+++ src/pages/ServicesPage.tsx
@@ -47,7 +47,7 @@ export const ServicesPage = () => {
         </div>
       </section>
 
-      <section className="box-border caret-transparent py-10 md:py-[60px]">
+      <section className="box-border caret-transparent py-10 md:py-[60px]" style={{ backgroundColor: '#e6feff' }}>
         <div className="box-border caret-transparent max-w-full w-full mx-auto px-5 md:max-w-[1336px]">
           <div className="box-border caret-transparent grid auto-cols-[1fr] grid-cols-[1fr] grid-rows-[auto] gap-y-10 md:grid-cols-[1fr_1fr] md:gap-x-[30px] md:gap-y-[60px]">
             {services.map((service) => (
