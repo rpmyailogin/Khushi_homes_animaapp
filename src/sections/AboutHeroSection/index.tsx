0c034d7cb0ed8a1d047159a58707739bc3ed78ea
Ritz <rp.myailogin@gmail.com>
2025-11-23 15:06:12 +1100

    Updated index.tsx

--- src/sections/AboutHeroSection/index.tsx
+++ src/sections/AboutHeroSection/index.tsx
@@ -1,9 +1,11 @@
 import { AboutHeroImage } from "@/sections/AboutHeroSection/components/AboutHeroImage";
+import { AboutHeroContent } from "@/sections/AboutHeroSection/components/AboutHeroContent";
 
 export const AboutHeroSection = () => {
   return (
     <section className="box-border caret-transparent overflow-hidden mb-[60px] md:mb-[150px]">
       <AboutHeroImage />
+      <AboutHeroContent />
     </section>
   );
 };
