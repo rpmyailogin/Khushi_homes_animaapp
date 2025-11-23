91913ba6476e99780f2a849320164d259d9af271
Ritz <rp.myailogin@gmail.com>
2025-11-23 16:44:47 +1100

    Updated FooterLinks.tsx

--- src/sections/Footer/components/FooterLinks.tsx
+++ src/sections/Footer/components/FooterLinks.tsx
@@ -2,8 +2,8 @@ import { FooterColumn } from "@/sections/Footer/components/FooterColumn";
 
 export const FooterLinks = () => {
   return (
-    <div className="items-start box-border caret-transparent flex flex-col justify-between gap-y-10 mt-10 md:flex-row md:gap-y-[normal]">
-      <div className="items-start box-border caret-transparent gap-x-[55px] grid auto-cols-[1fr] grid-cols-[1fr_1fr] grid-rows-[auto_auto] justify-between gap-y-[30px] md:gap-x-[140px] md:flex md:auto-cols-auto md:grid-cols-none md:grid-rows-none md:justify-start md:gap-y-[normal]">
+    <div className="items-start box-border caret-transparent flex flex-col justify-between gap-y-10 mt-10 md:flex-row md:gap-y-[normal] md:w-full">
+      <div className="items-start box-border caret-transparent gap-x-[55px] grid auto-cols-[1fr] grid-cols-[1fr_1fr] grid-rows-[auto_auto] justify-between gap-y-[30px] md:gap-x-[140px] md:flex md:auto-cols-auto md:grid-cols-none md:grid-rows-none md:justify-between md:gap-y-[normal] md:w-full">
         <FooterColumn
           title="Follow us"
           links={[
