ea72157b70de9017aa51241dd4bd3117984bb7d7
Ritz <rp.myailogin@gmail.com>
2026-04-07 19:56:30 +1000

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   publicDir: "./public",
-  base: "/",
+  base: "./",
   build: {
     outDir: "productionkhushi",
     emptyOutDir: true,
